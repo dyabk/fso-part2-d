989dda541a141fb31598a8e05f3797a2a4ba4942
dyabk <dzmitry.yabkevich@outlook.com>
2022-01-19 06:02:53 -0500

    Updated the state of the App component to render the new note to the screen

--- src/App.js
+++ src/App.js
@@ -25,18 +25,14 @@ const App = ( props ) => {
       content: newNote,
       date: new Date().toISOString(),
       important: Math.random() < 0.5,
-      //id: notes.length + 1
     }
 
     axios
       .post('http://localhost:3001/notes', noteObject)
       .then(response => {
-        console.log(response)
+        setNotes(notes.concat(response.data))
+        setNewNote('')
       })
-    
-
-    //setNotes(notes.concat(noteObject))
-    //setNewNote('')
   }
   
   const handleNoteChange = (event) => {
