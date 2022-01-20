0aa75f59d1230ad0f11a8213b0c7bd5dc8af2361
dyabk <dzmitry.yabkevich@outlook.com>
2022-01-20 02:53:38 -0500

    Fixed the bug in notes.js: fixed the parameter of the function of the update constant variable

--- src/services/notes.js
+++ src/services/notes.js
@@ -9,7 +9,7 @@ const create = ( newObject ) => {
     return axios.post(baseUrl, newObject)
 }
 
-const update = ( id, object ) => {
+const update = ( id, newObject ) => {
     return axios.put(`${baseUrl}/${id}`, newObject)
 }
 
